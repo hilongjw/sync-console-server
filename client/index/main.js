8a5b0f679303c16554e2c3592e78a98264c3310e
Awe <hilongjw@gmail.com>
2017-05-16 16:06:24 +0800

    add: index synccnsole sdk

--- client/index/main.js
+++ client/index/main.js
@@ -4,9 +4,23 @@ import './style/index.css'
 
 const app = new Vue({
     el: '#app',
+    data () {
+        return {
+            syncConsoleManager: null
+        }
+    },
     render: h => h(App),
     components: {
         App
+    },
+    mounted () {
+        this.syncConsoleManager = new window.SyncConsoleManager({
+            el: '#app',
+            // for sync console
+            project: 'im',
+            maxLogCount: 50,
+            server: 'http://sync.bood.in/',
+        })
     }
 })
 
